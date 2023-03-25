550531745741c0d6df1004d0fcd9ebbdfcfeb1e6
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2023-03-25 17:40:33 +0800

    Add jupyter notebook and scheduler

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ model = SimpleCNN()
 train_loader, val_loader = get_data_loaders(MODEL_INPUT_SHAPE, CENTRE_CROP, BATCH_SIZE)
 train(model, train_loader, val_loader, train_config)
 torch.save(model.state_dict(), f'{MODEL_SAVE_PATH}/model.pth')
+
+### TEST
+
 test_loader = get_test_loader(MODEL_INPUT_SHAPE, CENTRE_CROP, BATCH_SIZE)
 
-# model.load_state_dict(torch.load(f'{MODEL_SAVE_PATH}/model.pth'))
-# model = model.to('mps')
+model.load_state_dict(torch.load(f'{MODEL_SAVE_PATH}/model.pth'))
+model = model.to('mps')
 loss, acc = test(model, test_loader, train_config)
 print(f'overall test accuracy {acc}%')
